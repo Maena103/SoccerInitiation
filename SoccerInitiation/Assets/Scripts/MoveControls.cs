a308ee231d1379e3857e8f682034c915fd4018d6
Maena103 <maena.jenesse@gmail.com>
2023-10-25 12:36:56 -0400

    persos bougent

--- SoccerInitiation/Assets/Scripts/MoveControls.cs
+++ SoccerInitiation/Assets/Scripts/MoveControls.cs
@@ -3,22 +3,30 @@ using UnityEngine;
 public class MoveControls : MonoBehaviour
 {
     [SerializeField]
+    private KeyCode Foward = KeyCode.W;
+    [SerializeField]
+    private KeyCode Backward = KeyCode.S;
+    [SerializeField]
+    private KeyCode Left = KeyCode.A;
+    [SerializeField]
+    private KeyCode Right = KeyCode.D;
+    [SerializeField]
     private float speed = 2.0f;
     
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(Foward))
         {
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(Backward))
         { 
             transform.Translate(-Vector3.forward * Time.deltaTime * speed);
         }
-        if (Input.GetKey(KeyCode.A)) { 
+        if (Input.GetKey(Left)) { 
             transform.Rotate(0, -1, 0);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(Right))
         {
             transform.Rotate(0, 1, 0);
         }
